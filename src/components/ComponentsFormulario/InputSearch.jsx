54c4c05e8b4a10f1c45fb4c3b0321b699537136c
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2024-05-04 14:36:41 -0300

    gravação de voz ativo

--- src/components/ComponentsFormulario/InputSearch.jsx
+++ src/components/ComponentsFormulario/InputSearch.jsx
@@ -1,62 +1,84 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { IoMdMic } from "react-icons/io";
 
 
 const FormSearch = styled.form`
-    display: flex;
-    gap: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
 `;
 
 const InputFind = styled.input`
-    padding: 0.2rem 0.8rem;
-    border-radius: 4px;
-    border: none;
+  padding: 0.2rem 0.8rem;
+  border-radius: 4px;
+  border: none;
 `;
 
 const InputButton = styled.button`
-    background-color: #00a6ff;
-    border: 2px solid #f7fffd;
-    border-radius: 4px;
-    color: #000;
-    padding: 0.3rem;
-    font-size: 1.3rem;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    transition: 1s;
-  &:hover{
+  background-color: #00a6ff;
+  border: 2px solid #f7fffd;
+  border-radius: 4px;
+  color: #000;
+  padding: 0.3rem;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  transition: 1s;
+
+  &:hover {
     background-color: #a3f8f6;
     border: 2px solid #00a6ff;
-    }
+  }
 `;
+const BntRec = styled.button`
+
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+`
 
 const InputSearch = () => {
-    const [search, setSearch] = useState("");
-    const navigate = useNavigate();
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!search) return;
-      navigate(`/search?q=${search}`, { replace: true });
-      setSearch("");
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch('');
+  };
+
+  const startListening = () => {
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.lang = 'pt-BR';
+
+    recognition.onresult = (event) => {
+      const transcript = event.results[0][0].transcript;
+      setSearch(transcript);
     };
 
+    recognition.start();
+  };
 
   return (
     <FormSearch onSubmit={handleSubmit}>
-
-    <InputFind
-      type="text"
-      placeholder="Busque um filme"
-      onChange={(e) => setSearch(e.target.value)}
-      value={search}
-    />
-    <InputButton type="submit">
-      <BiSearchAlt2 />
-    </InputButton>
-  </FormSearch>
-  )
-}
+      <InputFind
+        type="text"
+        placeholder="Busque um filme"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      <InputButton type="submit">
+        <BiSearchAlt2 />
+      </InputButton>
+      <BntRec onClick={startListening}><IoMdMic size={20} /></BntRec>
+    </FormSearch>
+  );
+};
 
 export default InputSearch;
--- src/components/SpeechRecognitionText.jsx
+++ src/components/SpeechRecognitionText.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
-
 const SpeechRecognitionText = () => {
-  const { 
-    transcript,
-    resetTranscript,
-    browserSupportsSpeechRecognation
-  } = useSpeechRecognition()
+  // Usando o hook useSpeechRecognition para acessar funcionalidades de reconhecimento de fala
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
-  if(!browserSupportsSpeechRecognation) return (<span>Seu navegador não é compativel com a Pesquisa por voz</span>)
+  // Verifica se o navegador suporta o reconhecimento de fala
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Seu navegador não é compatível com a Pesquisa por voz</span>;
+  }
 
   return (
     <div>
-          
-    <button onClick={SpeechRecognition.startListening}>Fale</button>
-    <button onClick={SpeechRecognition.stopListening}>Pare</button>
-    <button onClick={resetTranscript}>Resetar campo de Texto</button>
+      {/* Botões para iniciar, parar e resetar o reconhecimento de fala */}
+      <button onClick={SpeechRecognition.startListening}>Fale</button>
+      <button onClick={SpeechRecognition.stopListening}>Pare</button>
+      <button onClick={resetTranscript}>Resetar campo de Texto</button>
 
-    <h1>{transcript}</h1>
+      {/* Exibe o texto transcritor da fala */}
+      <h1>{transcript}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default SpeechRecognitionText
+export default SpeechRecognitionText;
