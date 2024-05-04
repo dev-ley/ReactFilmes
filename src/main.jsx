a1b4b9c660700fa3bb7590e745dca2b72fb5b409
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2024-05-04 15:08:03 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={ <App />}>
-          <Route path='/*' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/movie/:id' element={<Movie />} />
-          <Route path='/search/*' element={<Search />} />
+          <Route path='/search/' element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
