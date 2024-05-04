55a860268e918cce69cd6e35d641b8a41f054422
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2024-05-04 14:47:30 -0300

    modal enquanto grava

--- src/components/ComponentsFormulario/InputSearch.jsx
+++ src/components/ComponentsFormulario/InputSearch.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { IoMdMic } from "react-icons/io";
-
+import { IoMdMic } from 'react-icons/io';
 
 const FormSearch = styled.form`
   display: flex;
@@ -33,33 +32,53 @@ const InputButton = styled.button`
     border: 2px solid #00a6ff;
   }
 `;
+
 const BntRec = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-display: flex;
-justify-content: center;
-align-items: center;
+const RecordingModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #a3f8f6;
+  padding: 20px;
+  border-radius: 8px;
+  font-size: 20px;
+  font-weight: 400; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
 
-
-`
-
-const InputSearch = () => {
-  const [search, setSearch] = useState('');
+const SpeechInput = () => {
+  const [spokenText, setSpokenText] = useState('');
+  const [listening, setListening] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!search) return;
-    navigate(`/search?q=${search}`, { replace: true });
-    setSearch('');
+    if (!spokenText) return;
+    navigate(`/search?q=${spokenText}`, { replace: true });
+    setSpokenText('');
   };
 
   const startListening = () => {
     const recognition = new window.webkitSpeechRecognition();
     recognition.lang = 'pt-BR';
 
+    recognition.onstart = () => {
+      setListening(true);
+    };
+
     recognition.onresult = (event) => {
       const transcript = event.results[0][0].transcript;
-      setSearch(transcript);
+      setSpokenText(transcript);
+    };
+
+    recognition.onend = () => {
+      setListening(false);
     };
 
     recognition.start();
@@ -70,15 +89,22 @@ const InputSearch = () => {
       <InputFind
         type="text"
         placeholder="Busque um filme"
-        onChange={(e) => setSearch(e.target.value)}
-        value={search}
+        value={spokenText}
+        onChange={(e) => setSpokenText(e.target.value)}
       />
       <InputButton type="submit">
         <BiSearchAlt2 />
       </InputButton>
-      <BntRec onClick={startListening}><IoMdMic size={20} /></BntRec>
+      <BntRec onClick={startListening}>
+        <IoMdMic size={20} />
+      </BntRec>
+      {listening && (
+        <RecordingModal>
+          <p>Gravando...</p>
+        </RecordingModal>
+      )}
     </FormSearch>
   );
 };
 
-export default InputSearch;
+export default SpeechInput;
